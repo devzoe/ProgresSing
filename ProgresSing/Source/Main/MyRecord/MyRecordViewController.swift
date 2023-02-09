df0576535f53fe68af66c26f5cd800ce489529aa
kyoungmin nam <58581608+devzoe@users.noreply.github.com>
2023-02-10 05:10:17 +0900

    feat : myrecord - navigation title

---
+++ ProgresSing/Source/Main/MyRecord/MyRecordViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MyRecordViewController.swift
+//  ProgresSing
+//
+//  Created by 남경민 on 2023/02/10.
+//
+
+import UIKit
+
+class MyRecordViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.navigationController?.navigationBar.topItem?.title = "목록"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
